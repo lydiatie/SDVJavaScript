b09ef5ea91de49c6fb4ab9bd0d6d114e7e3e26a8
Lydia Tie <lydiashinhong-tie@live.nmit.ac.nz>
2021-03-09 10:56:45 +1300

    week3 JS Variables

--- src/week3.js
+++ src/week3.js
@@ -1,2 +1,19 @@
-console.log('Hello SDV503')
-console.log('Lydia Tie')
+// TODO JavaScript Variables
+
+// Declare a variable first name and assign a value string
+var firstName = 'Lydia'
+// Print it
+console.log(firstName)
+
+const lastName = 'Tie'
+// Print it
+console.log(lastName)
+
+let fullName = firstName + ' ' + lastName //
+
+fullName = `My full name is ${firstName} ${lastName}` //ES6 Version
+
+let string = `Hello SDV503 class`
+
+console.log(fullName)
+console.log(string)
