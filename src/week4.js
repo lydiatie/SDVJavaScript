7d0030f7594cdb2aae5c87fc80571d060624093a
Lydia Tie <lydiashinhong-tie@live.nmit.ac.nz>
2021-03-15 12:49:02 +1300

    week4js added

---
+++ src/week4.js
@@ -0,0 +1,18 @@
+// Review
+/**
+Values & Variables
+ */
+// 
+let country = "New Zealand"
+let city = "Nelson"
+let postalCode = "7010"
+
+console.log(typeof country);
+console.log(city);
+console.log(typeof postalCode);
+
+let isIsland = true
+let language
+
+console.log(typeof isIsland);
+console.log(language)
