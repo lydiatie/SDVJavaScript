4750305a11a64718b919d34cc7aaf5f37ddc5b2a
Lydia Tie <lydiashinhong-tie@live.nmit.ac.nz>
2021-03-16 10:59:40 +1300

    Week4 practice

--- src/week4.js
+++ src/week4.js
@@ -59,3 +59,45 @@ if (typeof city === 'string') {
 else {
     console.log('This is false')
 }
+
+x = '1234567890';
+console.log(typeof x)
+let y = Number(x)
+
+console.log(typeof y);
+
+const a = 1 + 1
+console.log(a);
+
+const b = a + 2
+console.log(b);
+
+//object datatype
+const information = {
+    student : 'Lydia',
+    country : 'Malaysia',
+    city : 'Nelson',
+    postalCode : 7010,
+    age : 3
+}
+
+let c = 11
+
+if(c !== 11) {
+    console.log('its true')
+}
+else {
+    console.log('error')
+}
+
+let l = 5 // l has been assigned value 5
+let m = l // m has been assigned value l which is 5, line 93 memory deleted
+
+console.log(l)
+console.log(m)
+console.log(l === m)
+
+l = 10 // new value assigned to l, memory is now define l as 10
+console.log(l)
+console.log(m)
+console.log(l === m)
