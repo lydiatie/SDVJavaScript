4c56ec87c1eb8a83ae05872a27648b96e2425f12
Lydia Tie <lydiashinhong-tie@live.nmit.ac.nz>
2021-03-15 14:41:58 +1300

    week4js if else statement testing

--- src/week4.js
+++ src/week4.js
@@ -2,7 +2,7 @@
 /**
 Values & Variables
  */
-// 
+// Declared variables and added value to string
 let country = "New Zealand"
 let city = "Nelson"
 let postalCode = "7010"
@@ -17,4 +17,45 @@ let isIsland = true
 let language
 
 console.log(typeof isIsland);
+console.log(typeof language)
+
+language = "Arabic"
 console.log(language)
+
+let number = 10
+console.log(number)
+
+console.log(number / 2)
+console.log(number > 6)
+console.log(number < 2)
+
+const address = country + ' ' + city + ' ' + postalCode
+console.log(address)
+
+//string template
+let string = `My address is ${country}, ${city} and ${postalCode}`
+
+console.log(string)
+
+let x = 'Lydia';
+
+// && = and || = or
+if (x === 'Lydia' || typeof 'string') {
+    console.log('this is true')
+} else {
+    console.log('Error')
+}
+
+if (typeof x === 'number') {
+    console.log('this is true')
+}
+else {
+    console.log('Error')
+}
+
+if (typeof city === 'string') {
+    console.log('True. The city is Nelson')
+}
+else {
+    console.log('This is false')
+}
